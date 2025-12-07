b983c160a20fd5b64162a0013b64de0457d4c82e
tobexeon <46908308+tobexeon@users.noreply.github.com>
2025-12-07 23:43:42 +0800

    Update hmd_device_hooks.h

--- projects/psvr2_openvr_driver_ex/hmd_device_hooks.h
+++ projects/psvr2_openvr_driver_ex/hmd_device_hooks.h
@@ -8,6 +8,9 @@ namespace psvr2_toolkit {
   public:
     static void InstallHooks();
     static void UpdateGaze(void* pData, size_t dwSize);
+    // [新增] 公开给 IPC Server 调用的接口
+    static void EnableCalibration(bool enable);
+    static void ReloadCalibration();
   };
 
 } // psvr2_toolkit
